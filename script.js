884d799993dd629ad43ff25513dda2945534074e
Mary <mary_hbb@yahoo.com>
2023-05-10 22:29:03 +0200

    Implement functionas

---
+++ script.js
@@ -0,0 +1,25 @@
+function createPoint(x, y) {
+  const svgEL = document.querySelector("svg");
+  const circleEL = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "circle"
+  );
+  svgEL.appendChild(circleEL);
+  circleEL.setAttribute("cx", x);
+  circleEL.setAttribute("cy", y);
+  circleEL.setAttribute("fill", "blue");
+  circleEL.setAttribute("r", 5);
+}
+
+
+  function findXY(r, deg, offset) {
+      const rad = degToRad(deg);
+      let x = r * Math.cos(rad);
+      let y = r * Math.sin(rad);
+      if (offset) {
+        x = x + offset.x;
+        y = y + offset.y;
+      }
+      return { x, y };
+    }
+    
